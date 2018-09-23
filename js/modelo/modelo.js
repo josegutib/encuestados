d0bcc5a2a19666958bc9db1213142edc3dd04b5a
josefo <josemagut@hotmail.com.ar>
2018-09-23 13:13:52 -0300

    Creación de repo Encuestados en Github, Guía 1 en proceso

---
+++ js/modelo/modelo.js
@@ -0,0 +1,39 @@
+/*
+ * Modelo
+ */
+var Modelo = function() {
+  this.preguntas = [];
+  this.ultimoId = 0;
+
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this);
+};
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    let ultimoId = 0;
+    let self = this;
+    for(i=0; i<self.preguntas.length; i++){
+      if(self.preguntas[i].id>ultimoId){
+        ultimoId=self.preguntas[i].id;
+      }
+    }
+    return ultimoId;
+  },
+
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(nombre, respuestas) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    this.preguntas.push(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+
+  },
+
+  //se guardan las preguntas
+  guardar: function(){
+  },
+};
